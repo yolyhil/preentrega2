3566f7abab39482f3e376e867f9f3c67c16579bd
YolyOrtiz <112596306+yolyhil@users.noreply.github.com>
2024-11-13 23:18:21 -0300

    Create index.js
    file principal

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import  express  from "express";
+import cookieParser from 'cookie-parser';
+//Fix para __direname
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import {methods as authentication} from "./controllers/authentication.controller.js"
+import {methods as authorization} from "./middlewares/authorization.js";
+
+//Server
+const app = express();
+app.set("port",4000);
+app.listen(app.get("port"));
+console.log("Servidor corriendo en puerto",app.get("port"));
+
+//Configuración
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(cookieParser())
